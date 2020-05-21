ff0612d99a6c47ee423c1b2e7303a27b8f623e1a
brianvarskonst <brianvar.skonst@gmail.com>
2020-05-21 23:56:59 +0200

    Update: PHP CS rule only use PSR-12 for now

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -1,17 +1,18 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
     /**
      * @Route("/", name="app.index")
      *
-     * @psalm-return Response
      * @return Response
      */
     public function index(): Response
